eb2951a19eafcd7953a3164fbd9fe1ec3713b29f
adanjz <adanzweig@gmail.com>
2019-12-23 12:25:04 -0300

    changes locales

--- config/application.rb
+++ config/application.rb
@@ -130,7 +130,7 @@ module Greenlight
     # DEFAULTS
 
     # Default branding image if the user does not specify one
-    config.branding_image_default = "https://raw.githubusercontent.com/bigbluebutton/greenlight/master/app/assets/images/logo_with_text.png"
+    config.branding_image_default = "https://static.wixstatic.com/media/85087f_0d84cbeaeb824fca8f7ff18d7c9eaafd~mv2.png/v1/fill/w_160,h_30,al_c,q_85,usm_0.66_1.00_0.01/Logo_completo_Color_1PNG.webp"
 
     # Default primary color if the user does not specify one
     config.primary_color_default = "#467fcf"
